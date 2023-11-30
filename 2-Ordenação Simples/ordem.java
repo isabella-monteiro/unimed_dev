5ece5cd84114a455b69fe83a40895160e7ae80e2
isabella-monteiro <isa2013b@yahoo.com>
2023-11-29 22:09:57 -0300

    2-Ordenação Simples
    Questão resolvida com Java

---
+++ 2-Ordenação Simples/ordem.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class ordem {
+    public static void main(String[] args) {
+        int[] numbers = {9,15,3,7,8,20};
+        
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+        
+    }
+    
+}
